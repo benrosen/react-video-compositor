fa31f12fb16f83764537ad9e8c0ddef0f7fcf3af
Benjamin Rosen <benjamin.rosen@pearson.com>
2021-04-17 20:53:54 -0400

    implement useBodyPix hook

---
+++ src/hooks/useBodyPix.js
@@ -0,0 +1,18 @@
+import * as bodyPix from "@tensorflow-models/body-pix";
+
+import { useEffect, useState } from "react";
+
+/**
+ * Use TensorFlow's BodyPix module.
+ */
+export default function useBodyPix() {
+  const [net, setNet] = useState();
+
+  useEffect(() => {
+    bodyPix.load().then((net) => {
+      setNet(net);
+    });
+  }, []);
+
+  return [bodyPix, net];
+}
